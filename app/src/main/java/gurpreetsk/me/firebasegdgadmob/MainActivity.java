26fd37604a0490845246090afa507dae643b44e4
Gurpreet <gurpreetsk@hotmail.com>
2016-07-03 14:39:43 +0530

    -initial commit

---
+++ app/src/main/java/gurpreetsk/me/firebasegdgadmob/MainActivity.java
@@ -0,0 +1,24 @@
+package gurpreetsk.me.firebasegdgadmob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-2425943660599811~3924545881");    //initialise
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();      //request ad from server
+        mAdView.loadAd(adRequest);      //load ad
+
+    }
+}
